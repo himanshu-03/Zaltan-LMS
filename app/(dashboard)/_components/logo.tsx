b94822e9131f74663144f5936e0d18e92171a680
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2024-04-19 12:42:02 +0530

    Added Logos and Favicons (#2)
    * test
    
    * git sync
    
    * added logos and favicons

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,19 +1,25 @@
-import Link  from "next/link"
-import Image from "next/image"
-
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "../../../components/providers/theme-provider";
 
 export const Logo = () => {
+    const { theme } = useTheme();
+
+    const lightLogoSrc = "/light.png";
+    const darkLogoSrc = "/dark.png";
+
+    const logoSrc = theme === "light" ? darkLogoSrc : lightLogoSrc;
+
     return (
         <>
             <Link href="/">
                 <Image
-                
-                    src="/logo.jpg"
+                    src={logoSrc}
                     alt="Logo"
                     width={130}
                     height={130}
                 />
             </Link>
         </>
-    )
-}
+    );
+};

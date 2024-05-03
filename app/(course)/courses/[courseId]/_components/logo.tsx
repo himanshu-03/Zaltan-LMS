c795e564432dd29729d02e530d443a22b873b1bf
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2024-05-03 23:32:37 +0530

    Update logo.tsx

--- app/(course)/courses/[courseId]/_components/logo.tsx
+++ app/(course)/courses/[courseId]/_components/logo.tsx
@@ -1,14 +1,20 @@
-// logo.tsx
-
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "../../../components/providers/theme-provider";
 
 export const Logo = () => {
+    const { theme } = useTheme();
+
+    const lightLogoSrc = "/light.png";
+    const darkLogoSrc = "/dark.png";
+
+    const logoSrc = theme === "light" ? darkLogoSrc : lightLogoSrc;
+
     return (
         <>
             <Link href="/">
                 <Image
-                    src="/dark.png"
+                    src={logoSrc}
                     alt="Logo"
                     width={130}
                     height={130}

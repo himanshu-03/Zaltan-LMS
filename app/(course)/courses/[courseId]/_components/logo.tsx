ba44e7a0e1904348e1f9268a24dde68fa675cd3b
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2024-05-03 20:57:00 +0530

    fixed logo in courses

--- app/(course)/courses/[courseId]/_components/logo.tsx
+++ app/(course)/courses/[courseId]/_components/logo.tsx
@@ -2,21 +2,13 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useTheme } from "../../../../../components/providers/theme-provider";
 
 export const Logo = () => {
-    const { theme } = typeof window !== 'undefined' ? useTheme() : { theme: "light" };
-
-    const lightLogoSrc = "/light.png";
-    const darkLogoSrc = "/dark.png";
-
-    const logoSrc = theme === "light" ? darkLogoSrc : lightLogoSrc;
-
     return (
         <>
             <Link href="/">
                 <Image
-                    src={logoSrc}
+                    src="/dark.png"
                     alt="Logo"
                     width={130}
                     height={130}
